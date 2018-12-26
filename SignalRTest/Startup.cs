18355cc7bd03744cbaf576a4764993187f4cec97
Drioux Guidry <driouxgd@yahoo.com>
2018-12-25 21:16:20 -0700

    Configure SignalR in ASP.NET

--- SignalRTest/Startup.cs
+++ SignalRTest/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRTest.Hubs;
 
 namespace SignalRTest
 {
@@ -33,6 +34,7 @@ namespace SignalRTest
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +54,10 @@ namespace SignalRTest
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<MessageHub>("/messageHub");
+            });
             app.UseMvc();
         }
     }
