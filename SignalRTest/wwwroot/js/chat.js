4c21dd784871915aacc22f5e3870c2fe3c1a88f1
Drioux Guidry <driouxgd@yahoo.com>
2018-12-27 16:58:25 -0700

    Add spacing to chat.js file for readability

--- SignalRTest/wwwroot/js/chat.js
+++ SignalRTest/wwwroot/js/chat.js
@@ -22,10 +22,13 @@ connection.start().catch(function (err) {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
+
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
+
     event.preventDefault();
 });
