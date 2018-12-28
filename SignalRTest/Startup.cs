3868d32ec5b96c5e6f8a66bf1ca74ce18b6feaea
Drioux Guidry <driouxgd@yahoo.com>
2018-12-27 17:36:10 -0700

    Configured MS Local DB for WaterDbContext

--- SignalRTest/Startup.cs
+++ SignalRTest/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRTest.DataAccess;
 using SignalRTest.Hubs;
 
 namespace SignalRTest
@@ -32,9 +34,13 @@ namespace SignalRTest
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSignalR();
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=WaterPlsDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<WaterDbContext>
+                (options => options.UseSqlServer(connection));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
