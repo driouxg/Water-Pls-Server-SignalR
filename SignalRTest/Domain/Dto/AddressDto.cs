30c9079f9c31caf8358cbd7feba2731189d50dd7
Drioux Guidry <driouxgd@yahoo.com>
2019-01-24 20:31:56 -0700

    Added address dto

---
+++ SignalRTest/Domain/Dto/AddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Domain.Dto
+{
+    public class AddressDto
+    {
+        public int streetNumber { get; set; }
+        public string streetName { get; set; }
+        public string route { get; set; }
+        public string cityName { get; set; }
+        public string stateName { get; set; }
+    }
+}
