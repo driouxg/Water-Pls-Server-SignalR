698fa33a6f2e26f1746e51a9d76e6cd31f94eb64
Drioux Guidry <driouxgd@yahoo.com>
2019-02-10 14:24:56 -0700

    Added initial work for ASP.NET Core's identity management api

---
+++ SignalRTest/Domain/Entity/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRTest.Domain.Entity
+{
+    public class IEntity
+    {
+        int Id { get; }
+    }
+}
